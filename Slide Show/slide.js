d743bd9057a88f6eba4bdc05f42fd664e6cd2f4d
Abimbola Ikuerowo <ikuerowo13@gmail.com>
2023-01-11 13:39:10 -0500

    Slideshow

---
+++ Slide Show/slide.js
@@ -0,0 +1,26 @@
+$(window).on('load',function () {
+    "use strict"
+    const imageCount = $('#slider ul li').length;
+    const imageWidth = $('#slider ul li img').first().width();
+    const totalWidth = (imageWidth * imageCount) + 'px';
+    let leftPosition = 0;
+    let counter = 0;
+    $("#slider ul").css("width",totalWidth);
+    $("#next").click(function () {
+       counter++;
+       if (counter === imageCount) {
+        counter = 0;
+       };
+       leftPosition = `-${counter * imageWidth}px`;
+       $("#slider ul").animate({left: leftPosition}, 700);
+    });
+    $("#previous").click(function () {
+        counter--;
+        if (counter < 0) {
+         counter = imageCount -1;
+        };
+        leftPosition = `-${counter * imageWidth}px`;
+        $("#slider ul").animate({left: leftPosition}, 700); 
+    });    
+});
+
